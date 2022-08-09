b5bd777c1b9953febea6b32d1a40993099b92abe
brakisto <brakisto2015@gmail.com>
2022-08-09 21:30:44 +0100

    Alpha version 0.5.1 ready in PyPi

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-from pv_sizing import pv
+from mypackage.pv import PVProduction
 
 # PVProduction(irr_data=irradiation, load=load, tnoct=42, gamma=-0.36, panel_power=450, num_panel=num_panel,
 #                       fresnel_eff=fresnel_fixed)
+
+
+print(type(PVProduction()))
