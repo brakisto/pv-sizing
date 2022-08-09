c8ef6b877ac3e2abbfcc162703ec44afa8838551
brakisto <brakisto2015@gmail.com>
2022-08-09 21:13:33 +0100

    setup.cfg + setup.toml

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-from pv_sizing import pv, battery
+from pv_sizing import pv
+
+# PVProduction(irr_data=irradiation, load=load, tnoct=42, gamma=-0.36, panel_power=450, num_panel=num_panel,
+#                       fresnel_eff=fresnel_fixed)
